ad2dffbd119baeaee602db4ac01e3e5202cbed78
JemmaPhillips <jemma.phillips01@bbc.co.uk>
2026-06-19 16:03:57 +0100

    add for loop

--- js-experiment/experiment.js
+++ js-experiment/experiment.js
@@ -2,3 +2,15 @@ const monthlyRent = 500;
 
 const yearlyRent = monthlyRent * 12;
 console.log(yearlyRent);
+
+
+
+const timesToRepeat = 150;
+const character = "🎄";
+
+let xmasTree = "";
+for (let i = 0; i < timesToRepeat; i++) {
+    xmasTree = xmasTree + character;
+}
+
+console.log(xmasTree);
